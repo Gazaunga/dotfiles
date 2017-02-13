87498ec05aeba990792fccfd0202c2dd8b3922c1
Gazaunga <Gazaunga@gmail.com>
2017-02-13 18:37:20 -0500

    Update altscript.sh

--- altscript.sh
+++ altscript.sh
@@ -2,9 +2,10 @@
 git clone https://github.com/syl20bnr/spacemacs ~/.emacs.d
 mkdir {.config,Pictures,Downloads,Videos,Games,Documents,Python,Sphinx,Music,.startpages}
 touch ~/.mpd/{mpd.db,mpd.log,mpd.pid,mpdstate}
-pacaur -S --noconfirm --noedit xorg-xinit gpodder3 xmonad texlive-localmanager-git youtube-dl mpv inkscape mpd ncmpcpp discord python-pip krita qutebrowser suckless-tools emacs oblogout roxterm texlive-most texlive-lang udiskie rofi i3-lock-fancy-git fontforge typora texstudio clipgrab variety darktable xfce4-power-manager compton spacefm leafpad ttf-ms-fonts opera
+pacaur -S --noconfirm --noedit xorg-xinit gpodder3 xmonad sigil evince nitrogen pandoc todotxt trash-cli cylon rmlint i-nex-git texlive-localmanager-git htop powertop youtube-dl mpv inkscape cava mpd ncmpcpp discord python-pip krita qutebrowser suckless-tools emacs oblogout roxterm texlive-most texlive-lang udiskie rofi i3-lock-fancy-git fontforge typora texstudio clipgrab variety darktable xfce4-power-manager compton spacefm leafpad ttf-ms-fonts opera
 sudo pip install rainbowstream
 cp /etc/X11/xinit/xinitrc ~/.xinitrc
+echo "nitrogen --restore" >> ~/.xinitrc
 echo "exec xmonad" >> ~/.xinitrc
-echo "compton -cf" ~/.xinitrc
+echo "compton -cf" >> ~/.xinitrc
 xinit -- :1

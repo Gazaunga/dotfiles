41f78f99470caaa341c2385250437798b55bea07
Gazaunga <Gazaunga@gmail.com>
2017-02-14 07:05:42 -0500

    Update altscript.sh

--- altscript.sh
+++ altscript.sh
@@ -6,6 +6,7 @@ touch ~/.mpd/{mpd.db,mpd.log,mpd.pid,mpdstate}
 touch ~/.xmobar/.xmobarrc
 pacaur -S --noconfirm --noedit xorg-xinit gpodder3 xmonad sigil xmobar-git evince nitrogen pandoc todotxt trash-cli cylon rmlint i-nex-git texlive-localmanager-git htop powertop youtube-dl mpv inkscape cava mpd ncmpcpp discord python-pip krita qutebrowser dmenu emacs oblogout roxterm texlive-most texlive-lang udiskie rofi fontforge typora texstudio clipgrab variety darktable xfce4-power-manager compton spacefm leafpad ttf-ms-fonts opera
 sudo pip install rainbowstream
+cabal install xmobar --flags="all_extensions"
 cp /etc/X11/xinit/xinitrc ~/.xinitrc
 echo "nitrogen --restore" >> ~/.xinitrc
 echo "exec xmonad" >> ~/.xinitrc

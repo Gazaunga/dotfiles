3bdfafdfc86f294e98aa69d9437b3ff061cecfaf
Gazaunga <Gazaunga@gmail.com>
2017-02-14 03:23:13 -0500

    Update altscript.sh

--- altscript.sh
+++ altscript.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 git clone https://github.com/syl20bnr/spacemacs ~/.emacs.d
-mkdir {.config,Pictures,Downloads,Videos,Games,Documents,Python,Sphinx,Music,.startpages}
+mkdir {.config,Pictures,Downloads,.xmobar, Videos,Games,Documents,Python,Sphinx,Music,.startpages}
 touch ~/.mpd/{mpd.db,mpd.log,mpd.pid,mpdstate}
-touch ~/.xmobarrc
+touch ~/.xmobar/.xmobarrc
 pacaur -S --noconfirm --noedit xorg-xinit gpodder3 xmonad sigil xmobar-git evince nitrogen pandoc todotxt trash-cli cylon rmlint i-nex-git texlive-localmanager-git htop powertop youtube-dl mpv inkscape cava mpd ncmpcpp discord python-pip krita qutebrowser dmenu emacs oblogout roxterm texlive-most texlive-lang udiskie rofi fontforge typora texstudio clipgrab variety darktable xfce4-power-manager compton spacefm leafpad ttf-ms-fonts opera
 sudo pip install rainbowstream
 cp /etc/X11/xinit/xinitrc ~/.xinitrc

27ab18a8e0bccfac9ca5776d3115d1801bf5a697
Gazaunga <Gazaunga@gmail.com>
2017-02-15 07:31:25 -0500

    Update altscript2.sh

--- altscript2.sh
+++ altscript2.sh
@@ -3,57 +3,6 @@ sudo pacman -Syu --noconfirm -- needed
 git clone https://github.com/syl20bnr/spacemacs ~/.emacs.d
 mkdir{.config,.scripts,Python,Sphinx,Music,.startpages}
 touch ~/.mpd/{mpd.db,mpd.log,mpd.pid,mpdstate}
-packagelist=$(
-  menulibre # menu editor
-  xorg-xkill
-  xfce4-pulseaudio-plugin
-  xfce-polkit-git
-  gvfs # mounted volume manager
-  emacs
-  ttf-ms-fonts
-  ttf-droid
-  compiz
-  emerald
-  emerald-themes
-  fusion-icon
-  compiz-manager
-  gpodder3 # podcast application
-  sigil
-  evince
-  pandoc
-  todotxt # cli todo notes
-  trash-cli
-  cylon # maintenance and backups
-  rmlint # remove duplicates
-  i-nex-git # hardware details
-  texlive-localmanager-git
-  htop
-  powertop
-  youtube-dl
-  mpv
-  inkscape
-  cava
-  mpd
-  ncmpcpp
-  discord # chat
-  python-pip
-  krita
-  qutebrowser
-  dmenu
-  oblogout
-  roxterm
-  texlive-most
-  texlive-lang
-  context-minimals-git
-  rofi
-  fontforge
-  typora
-  texstudio
-  clipgrab # dl/process videos from the web
-  variety # gtk wallpaper manager
-  darktable
-  opera
-)
-pacaur -S --noconfirm --noedit ${packagelist[@]}
+pacaur -S --noconfirm --noedit menulibre xorg-xkill xfce4-pulseaudio-plugin xfce-polkit-git gvfs adapta-gtk-theme xbindkeys xbindkeys_config-gtk2 lxappearance emacs ttf-ms-fonts ttf-droid compiz emerald emerald-themes fusion-icon compiz-manager gpodder3 sigil evince pandoc todotxt trash-cli cylon rmlint i-nex-git texlive-localmanager-git htop powertop youtube-dl mpv inkscape cava mpv ncmpcpp discord python-pip krita qutebrowser dmenu oblogout roxterm texlive-most texlive-lang context-minimals-git rofi fontforge typora texstudio clipgrab variety darktable opera
 sudo pip install rainbowstream
-compiz --replace ccp
+compiz --replace ccp &

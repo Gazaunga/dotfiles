210cc0cd7f3dde972491c0523f9763fa269f0b3d
Gazaunga <Gazaunga@gmail.com>
2017-02-16 11:47:08 -0500

    Create exit-box.sh

---
+++ exit-box.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+# Multiple Exit Script using Zenity for non GDM installs
+# Requires zenity
+#
+######## This part is the Exit Type picker  ##########
+title="EXIT"
+exit_type=$(zenity  --width="200" --height="200" --text="What do you want to do?" \
+--title="$title" --list --radiolist --column '' --column ''   \
+    TRUE "Shutdown" \
+    FALSE "Reboot" \
+    FALSE "Logout" \
+    | sed 's/ max//g')
+
+######### This part takes the selection and applies it  #############
+case "$exit_type" in
+    Logout)
+        bspc quit
+        ;;
+    Reboot)
+        systemctl reboot
+        ;;
+    Shutdown)
+        systemctl poweroff
+        ;;
+    *)
+        exit 0
+        ;;
+esac

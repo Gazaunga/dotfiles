cc13d3d2911c71fc12456090791f87a7b5d81100
Gazaunga <Gazaunga@gmail.com>
2017-02-17 19:38:28 -0500

    Update altscript.sh

--- altscript.sh
+++ altscript.sh
@@ -8,12 +8,13 @@ wget https://raw.githubusercontent.com/Gazaunga/dotfiles/master/.Xresources
 wget https://raw.githubusercontent.com/Gazaunga/dotfiles/master/.screenrc
 wget https://github.com/torakiki/sejda/releases/download/v3.0.0.M11/sejda-console_3.0.0.M11-1_all.deb
 sudo dpkg -i sejda-console_3.0.0.M11-1_all.deb
-touch .ncmpcpp
-cd .ncmpcpp
-wget https://raw.githubusercontent.com/Gazaunga/dotfiles/master/config
-cd ~
+#touch .ncmpcpp
+#cd .ncmpcpp
+#wget https://raw.githubusercontent.com/Gazaunga/dotfiles/master/config
+#cd ~
 git clone https://github.com/syl20bnr/spacemacs ~/.emacs.d
-mkdir {.config,Pictures,Downloads,.xmobar, Videos,Games,Documents,Python,Sphinx,Music,.startpages}
+#mkdir {.config,Pictures,Downloads,.xmobar, Videos,Games,Documents,Python,Sphinx,Music,.startpages}
+mkdir ~/{.config,.xmobar,.startpages,.mpd}
 touch ~/.mpd/{mpd.db,mpd.log,mpd.pid,mpdstate}
 touch ~/.xmobar/.xmobarrc
 pacaur -S --noconfirm --noedit xorg-xinit gpodder3 xmonad sigil xmobar-git clerk-git evince nitrogen pandoc todotxt trash-cli cylon rmlint i-nex-git texlive-localmanager-git htop powertop youtube-dl mpv inkscape cava mpd ncmpcpp discord python-pip krita qutebrowser dmenu emacs oblogout roxterm texlive-most texlive-lang udiskie rofi fontforge typora texstudio clipgrab variety darktable xfce4-power-manager compton spacefm leafpad ttf-ms-fonts opera

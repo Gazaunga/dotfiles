d661b038ab3142e04e33d279c496033fcf668cb6
Gazaunga <Gazaunga@gmail.com>
2017-02-18 00:43:16 -0500

    Create i3.sh

---
+++ i3.sh
@@ -0,0 +1,31 @@
+sudo pacman -Syu
+wget https://raw.githubusercontent.com/Gazaunga/dotfiles/master/.Xdefaults
+wget https://raw.githubusercontent.com/Gazaunga/dotfiles/master/.Xresources
+wget https://raw.githubusercontent.com/Gazaunga/dotfiles/master/.bashrc
+wget https://raw.githubusercontent.com/Gazaunga/dotfiles/master/.profile
+# change .profile $term to termite
+mkdir .config
+cd .config
+mkdir termite
+cd ~/
+mkdir .i3
+mkdir i3blocks
+pacaur -S i3-gaps-git i3blocks-git dmenu rofi emacs termite compton nitrogen lxappearance pandoc typora mpv texlive-most cmake darktable
+cd .i3
+wget https://raw.githubusercontent.com/Gazaunga/dotfiles/master/.i3/config
+cd ~/.config/i3blocks
+wget https://github.com/Gazaunga/dotfiles/blob/master/.config/i3blocks/config
+wget https://github.com/Gazaunga/dotfiles/blob/master/.config/i3blocks/i3status.conf
+cd ~/.config/termite
+wget https://github.com/Gazaunga/dotfiles/blob/master/.config/termite/config
+cd ~/.config
+mkdir neofetch
+mkdir mpv
+mkdir cava
+wget https://raw.githubusercontent.com/Gazaunga/dotfiles/master/.config/neofetch/config
+cd ~/.config
+wget https://raw.githubusercontent.com/Gazaunga/dotfiles/master/.config/compton.conf
+cd ~/.config/mpv
+wget https://raw.githubusercontent.com/Gazaunga/dotfiles/master/.config/mpv/mpv.conf
+cd ~/.config/cava
+wget https://raw.githubusercontent.com/Gazaunga/dotfiles/master/.config/cava/config

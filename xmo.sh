b41d0a3230f02370fb126404e88f348dce849491
Gazaunga <Gazaunga@gmail.com>
2017-02-17 19:13:04 -0500

    Create xmo.sh

---
+++ xmo.sh
@@ -0,0 +1,26 @@
+echo "Updating..."
+sudo pacman -Syu
+echo "Loading Xmonad Config"
+cd ~/.xmonad
+wget https://raw.githubusercontent.com/Gazaunga/dotfiles/master/.xmonad/xmonad.hs
+wget https://raw.githubusercontent.com/Gazaunga/dotfiles/master/.xmonad/Config.hs
+wget https://raw.githubusercontent.com/Gazaunga/dotfiles/master/.xmonad/Hooks.hs
+wget https://raw.githubusercontent.com/Gazaunga/dotfiles/master/.xmonad/Layouts.hs
+wget https://raw.githubusercontent.com/Gazaunga/dotfiles/master/.xmonad/Scratch.hs
+wget https://raw.githubusercontent.com/Gazaunga/dotfiles/master/.xmonad/Themes.hs
+wget https://raw.githubusercontent.com/Gazaunga/dotfiles/master/.xmonad/Topics.hs
+wget https://github.com/Gazaunga/dotfiles/blob/master/.xmonad/Utils.hs
+wget https://raw.githubusercontent.com/Gazaunga/dotfiles/master/.xmonad/config.sh
+wget https://raw.githubusercontent.com/Gazaunga/dotfiles/master/.xmonad/conkyrc
+wget https://raw.githubusercontent.com/Gazaunga/dotfiles/master/.xmonad/dzenBar.sh
+wget https://raw.githubusercontent.com/Gazaunga/dotfiles/master/.xmonad/dzenCal.sh
+wget https://raw.githubusercontent.com/Gazaunga/dotfiles/master/.xmonad/dzenHardware.sh
+wget https://raw.githubusercontent.com/Gazaunga/dotfiles/master/.xmonad/dzenLog.sh
+wget https://raw.githubusercontent.com/Gazaunga/dotfiles/master/.xmonad/dzenMusic.sh
+wget https://raw.githubusercontent.com/Gazaunga/dotfiles/master/.xmonad/dzenNetwork.sh
+wget https://raw.githubusercontent.com/Gazaunga/dotfiles/master/.xmonad/dzenPacman.hs
+wget https://raw.githubusercontent.com/Gazaunga/dotfiles/master/.xmonad/mpdConkyrc
+wget https://raw.githubusercontent.com/Gazaunga/dotfiles/master/.xmonad/playPause.sh
+cd ~/
+sudo pacman -Syu
+"Your Xmonad-Box is ready!"

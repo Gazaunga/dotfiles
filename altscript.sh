9b7c3c4382c3102aa2523f25fada166069db901f
Gazaunga <Gazaunga@gmail.com>
2017-02-17 23:08:50 -0500

    Update altscript.sh

--- altscript.sh
+++ altscript.sh
@@ -14,7 +14,7 @@ sudo dpkg -i sejda-console_3.0.0.M11-1_all.deb
 #cd ~
 git clone https://github.com/syl20bnr/spacemacs ~/.emacs.d
 #mkdir {.config,Pictures,Downloads,.xmobar, Videos,Games,Documents,Python,Sphinx,Music,.startpages}
-mkdir ~/{.config,.xmobar,.startpages,.mpd}
+mkdir ~/{.config,.xmobar,.startpages,.mpd,bin}
 touch ~/.mpd/{mpd.db,mpd.log,mpd.pid,mpdstate}
 touch ~/.xmobar/.xmobarrc
 pacaur -S --noconfirm --noedit xorg-xinit gpodder3 xmonad sigil xmobar-git clerk-git evince nitrogen pandoc todotxt trash-cli cylon rmlint i-nex-git texlive-localmanager-git htop powertop youtube-dl mpv inkscape cava mpd ncmpcpp discord python-pip krita qutebrowser dmenu emacs oblogout roxterm texlive-most texlive-lang udiskie rofi fontforge typora texstudio clipgrab variety darktable xfce4-power-manager compton spacefm leafpad ttf-ms-fonts opera

cc452bba03d01e343c7aa26a65cc19b5aee1aa04
Gazaunga <Gazaunga@gmail.com>
2017-02-20 23:17:29 -0500

    Update suckless.sh

--- suckless/suckless.sh
+++ suckless/suckless.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
-
 sudo pacman -Syu
 mkdir dwm
 mkdir bin
+wget https://raw.githubusercontent.com/Gazaunga/dotfiles/master/suckless/.Xdefaults
+wget https://raw.githubusercontent.com/Gazaunga/dotfiles/master/suckless/.conkyrc
+wget https://raw.githubusercontent.com/Gazaunga/dotfiles/master/suckless/.xinitrc
 mkdir .config
 cd .config
 mkdir qutebrowser
@@ -19,15 +21,12 @@ cd termite
 wget https://raw.githubusercontent.com/Gazaunga/dotfiles/master/.config/termite/config2 -O config
 cd ~/
 cd dwm
-wget https://raw.githubusercontent.com/Gazaunga/dotfiles/master/dwm/alt/config.h
-wget https://raw.githubusercontent.com/Gazaunga/dotfiles/master/dwm/alt/dwm.c
-wget https://raw.githubusercontent.com/Gazaunga/dotfiles/master/dwm/alt/dwmstatus
+wget https://raw.githubusercontent.com/Gazaunga/dotfiles/master/suckless/config.h
 cd ~/
 wget https://raw.githubusercontent.com/Gazaunga/dotfiles/master/.bash_aliases
 wget https://raw.githubusercontent.com/Gazaunga/dotfiles/master/.bash_functions
 pacaur -S --noconfirm --noedit xorg-xinit dwm dmenu dzen2 conky-cli st emacs typora texlive-most texstudio pandoc surf gpodder3 sigil clerk-git xbindkeys okular thunar gvfs file-roller thunar-archive plugin thunar-shares-plugin thunar-volman ffmpegthumbnailer tumbler raw-thumbnailer gvfs-smb sshfs gnome-tweak-tools trash-cli todotxt cylon rmlint texlive-localmanager-git htop powertop inxi openssh mpv inkscape cava mpd ncmpcpp python-pip qutebrowser oblogout termite udiskie vifm fontforge variety compton leafpad ttf-ms-fonts
 sudo pip install rainbowstream glances ImageScraper
-echo "exec dwm" >> ~/.xinitrc
 echo "alias surf='tabbed -c surf -e &'" >> ~/.profile
 echo "Recompiling..."
 cd ~/dwm

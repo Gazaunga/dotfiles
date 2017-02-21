25acdc3bb8317f2f0bc8b9ea97d32fafb3ef0301
Gazaunga <Gazaunga@gmail.com>
2017-02-21 16:40:03 -0500

    Update xsession.sh

--- conf/xsession.sh
+++ conf/xsession.sh
@@ -1,1 +1,35 @@
+#!/bin/sh
+hname="$(uname -n)"
 
+printf 'Setting graphical editor...\n'
+if which emacs; then
+    printf 'Found spacemacs\n'
+    export EDITOR='emacs -fs'
+elif which vim; then
+    printf 'Found vim\n'
+    export EDITOR='vim'
+fi
+
+printf 'Loading default Xresources...\n'
+xrdb -merge "$HOME/conf/Xresources"
+if [ -f "$HOME/.Xresources" ]; then
+    printf 'Loading %s/.Xresources...\n' "$HOME"
+    xrdb -merge "$HOME/.Xresources"
+fi
+
+printf 'Searching for xbindkeys...\n'
+if which xbindkeys; then
+    printf 'Running xbindkeys...\n'
+    xbindkeys
+fi
+
+if which redshift; then
+    printf 'Launching redshift...\n'
+    redshift &
+    redpid=$!
+fi
+
+if [ -x "$HOME/xsession.sh" ]; then
+    printf 'Running xsession.sh for %s...\n' "$hname"
+    . "$HOME/xsession.sh"
+fi

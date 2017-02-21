c4ae7633968725c847d3439df278b1676b66e3a3
Gazaunga <Gazaunga@gmail.com>
2017-02-21 06:50:20 -0500

    Update i3.sh

--- i3.sh
+++ i3.sh
@@ -18,7 +18,7 @@ mkdir i3blocks
 cd ~/.config/i3blocks
 wget https://github.com/Gazaunga/dotfiles/blob/master/.config/i3blocks/config
 wget https://github.com/Gazaunga/dotfiles/blob/master/.config/i3blocks/i3status.conf
-pacaur -S xorg-xinit i3-gaps-git i3blocks-git dmenu rofi emacs polkit-kde-agent-1 termite compton gsimplecal thunar gvfs file-roller thunar-archive plugin thunar-shares-plugin thunar-volman ffmpegthumbnailer tumbler raw-thumbnailer gvfs-smb sshfs nitrogen lxappearance pandoc typora xfce4-taskmanager mpv texlive-most cmake darktable
+pacaur -S xorg-xinit i3-gaps-git i3blocks-git dmenu rofi emacs polkit-kde-agent-1 termite compton gsimplecal thunar gvfs file-roller thunar-archive-plugin thunar-shares-plugin thunar-volman ffmpegthumbnailer tumbler raw-thumbnailer gvfs-smb sshfs nitrogen lxappearance pandoc typora xfce4-taskmanager mpv texlive-most cmake darktable
 cd ~/.i3
 wget https://raw.githubusercontent.com/Gazaunga/dotfiles/master/.i3/config -O config
 cd ~/.config/termite

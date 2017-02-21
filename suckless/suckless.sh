344c5f0116980cf671492e21d9c3e1dbeef1af79
Gazaunga <Gazaunga@gmail.com>
2017-02-20 23:12:20 -0500

    Update suckless.sh

--- suckless/suckless.sh
+++ suckless/suckless.sh
@@ -25,7 +25,7 @@ wget https://raw.githubusercontent.com/Gazaunga/dotfiles/master/dwm/alt/dwmstatu
 cd ~/
 wget https://raw.githubusercontent.com/Gazaunga/dotfiles/master/.bash_aliases
 wget https://raw.githubusercontent.com/Gazaunga/dotfiles/master/.bash_functions
-pacaur -S --noconfirm --noedit xorg-xinit dwm dmenu conky-cli st emacs typora texlive-most texstudio pandoc surf gpodder3 sigil clerk-git xbindkeys okular thunar gvfs file-roller thunar-archive plugin thunar-shares-plugin thunar-volman ffmpegthumbnailer tumbler raw-thumbnailer gvfs-smb sshfs gnome-tweak-tools trash-cli todotxt cylon rmlint texlive-localmanager-git htop powertop inxi openssh mpv inkscape cava mpd ncmpcpp python-pip qutebrowser oblogout termite udiskie vifm fontforge variety compton leafpad ttf-ms-fonts
+pacaur -S --noconfirm --noedit xorg-xinit dwm dmenu dzen2 conky-cli st emacs typora texlive-most texstudio pandoc surf gpodder3 sigil clerk-git xbindkeys okular thunar gvfs file-roller thunar-archive plugin thunar-shares-plugin thunar-volman ffmpegthumbnailer tumbler raw-thumbnailer gvfs-smb sshfs gnome-tweak-tools trash-cli todotxt cylon rmlint texlive-localmanager-git htop powertop inxi openssh mpv inkscape cava mpd ncmpcpp python-pip qutebrowser oblogout termite udiskie vifm fontforge variety compton leafpad ttf-ms-fonts
 sudo pip install rainbowstream glances ImageScraper
 echo "exec dwm" >> ~/.xinitrc
 echo "alias surf='tabbed -c surf -e &'" >> ~/.profile

67a15247f971ae508f152923429262e7a7b2d15c
Gazaunga <Gazaunga@gmail.com>
2017-02-20 23:34:45 -0500

    Update suckless.sh

--- suckless/suckless.sh
+++ suckless/suckless.sh
@@ -2,17 +2,17 @@
 sudo pacman -Syu
 mkdir dwm
 mkdir bin
-wget https://raw.githubusercontent.com/Gazaunga/dotfiles/master/suckless/.Xdefaults
-wget https://raw.githubusercontent.com/Gazaunga/dotfiles/master/suckless/.conkyrc
-wget https://raw.githubusercontent.com/Gazaunga/dotfiles/master/suckless/.xinitrc
+wget https://raw.githubusercontent.com/Gazaunga/dotfiles/master/suckless/.Xdefaults -O .Xdefaults
+wget https://raw.githubusercontent.com/Gazaunga/dotfiles/master/suckless/.conkyrc -O .conkyrc
+wget https://raw.githubusercontent.com/Gazaunga/dotfiles/master/suckless/.xinitrc -O .xinitrc
 mkdir .config
 cd .config
 mkdir qutebrowser
 cd ~/qutebrowser
 wget https://raw.githubusercontent.com/Gazaunga/dotfiles/master/.config/qutebrowser/qutebrowser.conf
 cd ~/
-wget https://raw.githubusercontent.com/Gazaunga/dotfiles/master/.profile
-wget https://raw.githubusercontent.com/Gazaunga/dotfiles/master/.Xresources
+wget https://raw.githubusercontent.com/Gazaunga/dotfiles/master/.profile -O .profile
+wget https://raw.githubusercontent.com/Gazaunga/dotfiles/master/.Xresources -O .Xresources
 wget https://raw.githubusercontent.com/Gazaunga/dotfiles/master/.bashrc -O .bashrc
 wget https://raw.githubusercontent.com/Gazaunga/dotfiles/master/.screenrc
 cd .config
@@ -21,7 +21,7 @@ cd termite
 wget https://raw.githubusercontent.com/Gazaunga/dotfiles/master/.config/termite/config2 -O config
 cd ~/
 cd dwm
-wget https://raw.githubusercontent.com/Gazaunga/dotfiles/master/suckless/config.h
+wget https://raw.githubusercontent.com/Gazaunga/dotfiles/master/suckless/config.h -O config.h
 cd ~/
 wget https://raw.githubusercontent.com/Gazaunga/dotfiles/master/.bash_aliases
 wget https://raw.githubusercontent.com/Gazaunga/dotfiles/master/.bash_functions

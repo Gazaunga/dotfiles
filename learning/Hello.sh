db521121e109a2ad672f6dc251a1ccdf4edc1cb7
Gazaunga <Gazaunga@gmail.com>
2017-02-28 13:26:56 -0500

    Create Hello.sh

---
+++ learning/Hello.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+echo Hello, who are you?
+
+echo ===================
+
+read -p 'Username: ' name
+
+echo Nice to mee you $name
+
+echo What is your password?
+
+read -sp 'Password: ' pass
+
+echo Thankyou $name we now have your login details
+
+

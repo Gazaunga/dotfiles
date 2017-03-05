c9e3e3968c6175bb573f58968259619d9d6a7322
Gazaunga <Gazaunga@gmail.com>
2017-03-04 20:02:35 -0500

    Update xmo.sh

--- xmo.sh
+++ xmo.sh
@@ -1,27 +1,33 @@
-echo "Updating..."
+#!/bin/bash
 sudo pacman -Syu --noconfirm --needed
-echo "Loading Xmonad Config"
-cd ~/.xmonad
-wget https://raw.githubusercontent.com/Gazaunga/dotfiles/master/.xmonad/xmonad.hs
-wget https://raw.githubusercontent.com/Gazaunga/dotfiles/master/.xmonad/Config.hs
-wget https://raw.githubusercontent.com/Gazaunga/dotfiles/master/.xmonad/Hooks.hs
-wget https://raw.githubusercontent.com/Gazaunga/dotfiles/master/.xmonad/Layouts.hs
-wget https://raw.githubusercontent.com/Gazaunga/dotfiles/master/.xmonad/Scratch.hs
-wget https://raw.githubusercontent.com/Gazaunga/dotfiles/master/.xmonad/Themes.hs
-wget https://raw.githubusercontent.com/Gazaunga/dotfiles/master/.xmonad/Topics.hs
-wget https://github.com/Gazaunga/dotfiles/blob/master/.xmonad/Utils.hs
-wget https://raw.githubusercontent.com/Gazaunga/dotfiles/master/.xmonad/config.sh
-wget https://raw.githubusercontent.com/Gazaunga/dotfiles/master/.xmonad/conkyrc
-wget https://raw.githubusercontent.com/Gazaunga/dotfiles/master/.xmonad/dzenBar.sh
-wget https://raw.githubusercontent.com/Gazaunga/dotfiles/master/.xmonad/dzenCal.sh
-wget https://raw.githubusercontent.com/Gazaunga/dotfiles/master/.xmonad/dzenHardware.sh
-wget https://raw.githubusercontent.com/Gazaunga/dotfiles/master/.xmonad/dzenLog.sh
-wget https://raw.githubusercontent.com/Gazaunga/dotfiles/master/.xmonad/dzenMusic.sh
-wget https://raw.githubusercontent.com/Gazaunga/dotfiles/master/.xmonad/dzenNetwork.sh
-wget https://raw.githubusercontent.com/Gazaunga/dotfiles/master/.xmonad/dzenPacman.hs
-wget https://raw.githubusercontent.com/Gazaunga/dotfiles/master/.xmonad/mpdConkyrc
-wget https://raw.githubusercontent.com/Gazaunga/dotfiles/master/.xmonad/playPause.sh
 cd ~/
+git clone https://github.com/syl20bnr/spacemacs ~/.emacs.d
+sudo pacman -S xmonad xmonad-contrib xmobar stalonetray dmenu scrot \
+    cabal-install xcompmgr screen emacs leafpad nitrogen krita texlive-most \
+		spacefm texstudio
+sudo cabal update
+cabal install --global yeganesh
+cd ~/
+mv .xmonad .xmonad.orig
+git clone https://github.com/Gazaunga/xmonad-config.git .xmonad
+
+# Fonts
+mkdir .fonts
+cd .fonts
+git clone https://github.com/be5invis/Iosevka.git
+git clone https://github.com/tonsky/FiraCode.git
+git clone https://github.com/sunaku/tamzen-font.git
+git clone https://github.com/adobe-fonts/source-code-pro.git
+
+# Bashrc
+cd ~/
+git clone https://github.com/nojhan/liquidprompt.git
+cat "source liquidprompt/liquidprompt" >> .bashrc
+
+cd ~/
+wge thttps://raw.githubusercontent.com/Gazaunga/DWM/master/bin/utils.sh
+https://raw.githubusercontent.com/Gazaunga/DWM/master/.Xdefaults
+wget https://raw.githubusercontent.com/Gazaunga/DWM/master/.bash_functions
+wget https://raw.githubusercontent.com/Gazaunga/DWM/master/.profile
+wget https://raw.githubusercontent.com/Gazaunga/DWM/master/.screenrc
 sudo pacman -Syu --noconfirm --needed
-echo "Your Xmonad-Box is ready!"
-reboot

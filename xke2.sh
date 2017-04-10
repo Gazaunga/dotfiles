9d9c99f3fbc85fb93976093df49360666029cca8
Gazaunga <Gazaunga@gmail.com>
2017-04-10 19:40:49 -0400

    Create xke2.sh

---
+++ xke2.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+sudo pacman -Syu --noconfirm -- needed
+pacaur -S --noconfirm --noedit xmonad xmonad-contrib xmobar stalonetray dmenu scrot \
+    cabal-install xcompmgr
+sudo cabal update
+cabal install --global yeganesh
+pacaur -S --noconfirm --noedit xorg xorg-xinit \
+  compton \
+  ttf-hack-powerline-git ttf-hack-ibx otf-fontawesome \
+  libreoffice \
+  scrot \
+  howdoi \
+  rxvt-unicode \
+  fontforge \
+  pandoc \
+  typora \
+  redshift \
+  texlive-most \
+  kdm \
+  plasma5-applets-eventcalendar \
+  kdeplasma-applets-fancytasks \
+  plasma5-applets-redshift-control-git \
+  plasma5-applet-netctl-gui \
+  kdeplasma-applets-veromix-git \
+  plasma5-applets-thermal-monitor-git \
+  plasma5-applets-active-window-control-git \
+  plasma5-applets-keystate \
+  plasma5-applets-resources-monitor-git \
+  kde-applications-meta
+cd
+mv .xmonad .xmonad.orig
+git clone https://github.com/Gazaunga/xmonad-config.git .xmonad
+reboot
